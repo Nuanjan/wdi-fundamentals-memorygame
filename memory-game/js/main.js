23610fcc7404c06be57b74dad84502d0d41eed38
Nuanjan <upbz_n.n_suju@hotmail.com>
2020-03-29 02:53:56 -0400

    created ame logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,14 @@
-console.log("Up and running!");
-
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-
-console.log(cardOne+cardThree);
+const card = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = card[0];
+let cardTwo = card[2];
+cardsInPlay.push(cardOne);
+console.log("the user flipped "+cardOne);
+cardsInPlay.push(cardTwo);
+console.log("the user flipped "+cardTwo);
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	}else
+	alert("Sorry, try again");
+}
